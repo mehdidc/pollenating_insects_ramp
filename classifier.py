bb596241034fb9652de5e5948400907e81af9827
mehdi <mehdi@cherti.name>
2015-10-08 00:57:10 +0200

    googlenet

--- classifier.py
+++ classifier.py
@@ -2,21 +2,19 @@ import os
 os.environ["THEANO_FLAGS"] = "device=gpu"
 
 from sklearn.pipeline import make_pipeline
-from caffezoo.vgg import VGG
+from caffezoo.googlenet import GoogleNet
 from sklearn.base import BaseEstimator
 from sklearn.ensemble import RandomForestClassifier
 
-
 """
-More details on VGG : http://arxiv.org/pdf/1409.1556.pdf
+More details on Google net : http://arxiv.org/abs/1409.4842
 
 layer_names can accept a list of strings, each string
 is a layer name. The complete list of layer names
 can be seen in this graph :
 
-    https://drive.google.com/open?id=0B1CFqLHwhNoaODJOOEV5M1ZNbWc
-
-Each node is either a convolution, a pooling layer or a nonlinearity layer.
+Each node is either a convolution, a pooling layer or a nonlinearity layer, or 
+other different kinds of layers.
 The nodes representing convolution and pooling layers start by
 the layer name (which you can put in layer_names). For convolutional layer
 if you just use the name of the layer, like "conv_1" it will only take
@@ -28,7 +26,7 @@ You can also provide an aggregation function, which takes
 a set of layers features and returns a numpy array. The default
 aggregation function used concatenate all the layers.
 
-VGG(aggregate_function=your_function, layer_names=[...])
+GoogleNet(aggregate_function=your_function, layer_names=[...])
 
 the default aggregation function looks like this:
     def concat(layers):
@@ -37,12 +35,11 @@ the default aggregation function looks like this:
 
 """
 
-
 class Classifier(BaseEstimator):
-
+ 
     def __init__(self):
         self.clf = make_pipeline(
-            VGG(layer_names=["pool3"]),
+            GoogleNet(layer_names=["inception_3b/output"]),
             RandomForestClassifier(n_estimators=100, max_depth=25)
         )
         
